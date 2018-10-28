3c4953c55a326db3e28f9992805e5722a41bb47c
Florent Béjina <florent@bejina.fr>
2018-10-28 11:57:37 +0100

    add some configuration to explress (cors, body-parser and logging)

--- config.sample.js
+++ config.sample.js
@@ -1,6 +1,9 @@
 const config = {
   NODE_ENV: 'dev', // or 'production'
   APP_PORT: 3000, // or any port you'd like
+  ENTRY_POINT: '/', // or any route you want ie: /email/receive
+  LOG_IN_FILE: true,
+  LOG_DIRECTORY: 'log',
 };
 
 module.exports = config;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
 
-import { APP_PORT } from '../config';
+import createLogStream from './utils/logStream';
 
-const App = express();
+import {
+  NODE_ENV, APP_PORT, ENTRY_POINT, LOG_IN_FILE,
+} from '../config';
 
-App.get('/', (req, res) => res.send('OK'));
+const app = express();
 
-App.listen(APP_PORT, () => console.log(`[SERVER] is running at port ${APP_PORT}`));
+// body parser config
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// cors config
+app.use(cors());
+
+// morgan configuration
+if (NODE_ENV !== 'production') {
+  app.use(morgan('dev'));
+}
+if (LOG_IN_FILE) {
+  const logStream = createLogStream();
+  app.use(morgan('combined', { stream: logStream }));
+}
+
+app.post(ENTRY_POINT, (req, res) => res.send('OK'));
+
+app.listen(APP_PORT, () => console.log(`[SERVER] is running at port ${APP_PORT}`));
