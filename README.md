c298269552b5ea5b180e69f1a8993626decec28c
Florent Béjina <florent@bejina.fr>
2018-10-28 11:11:24 +0100

    add babel config and simple express server

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # primost
 
 ```shell
+cp config.sample.js config.js
+
 yarn
 yarn dev
 ```
--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-const foo = 'bar';
-console.log(foo, foo, foo);
+import express from 'express';
+
+import { APP_PORT } from '../config';
+
+const App = express();
+
+App.get('/', (req, res) => res.send('OK'));
+
+App.listen(APP_PORT, () => console.log(`[SERVER] is running at port ${APP_PORT}`));
